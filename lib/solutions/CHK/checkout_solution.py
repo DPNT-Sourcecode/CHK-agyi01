b6ab3c1d5f04e2c3b850da44f74a26ff41fe655e
sbx_user1051 <sbx_user1051@169.254.13.13>
2023-10-17 20:08:13 +0000

    Tue Oct 17 20:08:13 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,12 +15,20 @@ def extract_prices_from_txt(filename):
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    filename = ''
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40,
-              'F': 10, 'G': 20, 'H': 35, 'I': 15, 'J': 40,}
+    filename = 'challenges/CHK_R4.txt'
+    prices = extract_prices_from_txt(filename)
+    # prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40,
+    #           'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60,}
     offers = {'A': [(5, 200), (3, 130)],
               'B': [(2, 45)],
-              'F': [(3, 20)]}
+              'F': [(3, 20)],
+              'H': [(10, 80), (5, 45)],
+              'K': [(2, 150)],
+              'P': [(5, 200)],
+              'Q': [(3, 80)],
+              'U': [(3, 80)],
+              'V': [(3, 130), (2, 90)]
+              }
     skusCount = Counter(skus)
 
     # check illegal input
@@ -32,8 +40,18 @@ def checkout(skus):
 
     # fix the number of item for checkout firstly via '2E get one B free'
     if 'E' in skusCount and skusCount['E'] >= 2:
-        countFree = skusCount['E'] // 2
-        skusCount['B'] = max(0, skusCount['B'] - countFree)
+        countFreeB = skusCount['E'] // 2
+        skusCount['B'] = max(0, skusCount['B'] - countFreeB)
+
+    # fix the number of item for checkout firstly via '3N get one M free'
+    if 'N' in skusCount and skusCount['N'] >= 3:
+        countFreeM = skusCount['N'] // 3
+        skusCount['M'] = max(0, skusCount['M'] - countFreeM)
+
+    # fix the number of item for checkout firstly via '3R get one Q free'
+    if 'R' in skusCount and skusCount['R'] >= 3:
+        countFreeQ = skusCount['R'] // 3
+        skusCount['Q'] = max(0, skusCount['Q'] - countFreeQ)
 
     for sku, count in skusCount.items():
         price = prices[sku]
@@ -47,3 +65,4 @@ def checkout(skus):
             total += count * price
 
     return total
+
