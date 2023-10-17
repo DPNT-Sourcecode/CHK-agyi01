b6e0f6feda85de3fadcd3e48bf66e33e76044749
sbx_user1051 <sbx_user1051@169.254.13.13>
2023-10-17 19:19:44 +0000

    Tue Oct 17 19:19:44 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,8 +6,8 @@ from collections import Counter
 def checkout(skus):
 
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-    offers = {'A': [(3, 130), (5, 200)], 'B': [(2, 45)], 'E':[(2, 0)]}
-    itemCount = Counter(skus)
+    offers = {'A': [(3, 130), (5, 200)], 'B': [(2, 45)], 'E': [(2, 0)]}
+    skusCount = Counter(skus)
 
     # check illegal input
     for k in skus:
@@ -15,20 +15,21 @@ def checkout(skus):
             return -1
 
     total = 0
-    for sku, count in itemCount.items():
+    for sku, count in skusCount.items():
         price = prices[sku]
 
         if sku in offers:
             for offerCount, offerPrice in sorted(offers[sku], reverse=True):  # get the best offer by sorting
                 while count >= offerCount:
                     if sku == 'E' and offerPrice == 0:  # 2E get one B free
-                        itemCount['B'] = max(0, itemCount['B'] - 1)
+                        skusCount['B'] = max(0, skusCount['B'] - 1)
                     else:
                         total += offerPrice
                     count -= offerCount
-                itemCount[sku] = count
-                
+                skusCount[sku] = count
+
     total += count * price
 
     return total
+
 
