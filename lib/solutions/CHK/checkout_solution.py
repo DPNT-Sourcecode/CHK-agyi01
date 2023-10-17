4db8ff6d68d96c3808478c8df6d3c47c9376620d
sbx_user1051 <sbx_user1051@169.254.13.13>
2023-10-17 18:51:39 +0000

    Tue Oct 17 18:51:39 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@ from collections import Counter
 # skus = unicode string
 def checkout(skus):
 
-    prices = {'A': 50, 'B': 30, 'C': '20', 'D': 15}
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     offers = {'A': (3, 130), 'B': (2, 45)}
     itemCount = Counter(skus)
 
@@ -25,3 +25,4 @@ def checkout(skus):
             total += count * price
 
     return total
+
