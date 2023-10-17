026f652775de3ed011a8a51cc619b7831278e369
sbx_user1051 <sbx_user1051@169.254.13.13>
2023-10-17 18:49:12 +0000

    Tue Oct 17 18:49:12 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,14 +1,30 @@
 from collections import Counter
 
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    price = 0
+    
+    prices = {'A': 50, 'B': 30, 'C': '20', 'D': 15}
+    offers = {'A': (3, 130), 'B': (2, 45)}
     itemCount = Counter(skus)
 
+    # check illegal input
+    for k in skus:
+        if k not in prices:
+            return -1
 
+    total = 0
+    for sku, count in itemCount.items():
+        price = prices[sku]
+        if sku in offers:
+            offerCount, offerPrice = offers[sku]
+            total += (count // offerCount) * offerPrice
+            total += (count % offerCount) * price
+        else:
+            total += count * price
 
+    return total
 
-    return price
 
 
